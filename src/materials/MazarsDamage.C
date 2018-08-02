d035e8d026463675f59c7514495def8584136608
Giorla, Alain B <giorlaab@ornl.gov>
2018-08-02 10:57:43 -0400

    moved MazarsDamage and ConcreteASRElasticityTensor to ScalarDamageBase ref #44

--- src/materials/MazarsDamage.C
+++ src/materials/MazarsDamage.C
@@ -27,12 +27,8 @@ template <>
 InputParameters
 validParams<MazarsDamage>()
 {
-  InputParameters params = validParams<DamageBase>();
+  InputParameters params = validParams<ScalarDamageBase>();
   params.addClassDescription("Mazars scalar damage model");
-  params.addParam<bool>(
-      "use_old_damage",
-      false,
-      "Whether to use the damage index from the previous step in the stress computation");
   params.addRequiredCoupledVar("tensile_strength",
                                "Tensile stress threshold for damage initiation");
   params.addRequiredParam<Real>("a_t",
@@ -43,49 +39,33 @@ validParams<MazarsDamage>()
       "a_c", "A_c parameter that controls the shape of the response in compression");
   params.addRequiredParam<Real>(
       "b_c", "B_c parameter that controls the shape of the response in compression");
-  params.addRangeCheckedParam<Real>(
-      "residual_stiffness_fraction",
-      1.e-8,
-      "residual_stiffness_fraction>=0 & residual_stiffness_fraction<1",
-      "Minimum fraction of original material stiffness retained for fully "
-      "damaged material (when damage_index=1)");
-  params.addRangeCheckedParam<Real>(
-      "maximum_damage_increment",
-      0.1,
-      "maximum_damage_increment>0 & maximum_damage_increment<1",
-      "maximum damage increment allowed for simulations with adaptative time step");
   return params;
 }
 
 MazarsDamage::MazarsDamage(const InputParameters & parameters)
-  : DamageBase(parameters),
+  : ScalarDamageBase(parameters),
     GuaranteeConsumer(this),
-    _use_old_damage(getParam<bool>("use_old_damage")),
     _tensile_strength(coupledValue("tensile_strength")),
     _a_t(getParam<Real>("a_t")),
     _a_c(getParam<Real>("a_c")),
     _b_t(getParam<Real>("b_t")),
     _b_c(getParam<Real>("b_c")),
-    _residual_stiffness_fraction(getParam<Real>("residual_stiffness_fraction")),
     _kappa(declareProperty<Real>(_base_name + "kappa")),
-    _kappa_old(getMaterialPropertyOld<Real>("kappa")),
-    _damage_index(declareProperty<Real>(_base_name + "damage_index")),
-    _damage_index_old(getMaterialPropertyOld<Real>("damage_index")),
+    _kappa_old(getMaterialPropertyOld<Real>(_base_name + "kappa")),
     _stress(getMaterialProperty<RankTwoTensor>(_base_name + "stress")),
     _mechanical_strain(getMaterialProperty<RankTwoTensor>(_base_name + "mechanical_strain")),
     _elasticity_tensor_name(_base_name + "elasticity_tensor"),
     _elasticity_tensor(getMaterialPropertyByName<RankFourTensor>(_elasticity_tensor_name)),
     _eigval(3, 0.0),
     _positive_stress(3, 0.0),
-    _positive_strain(3, 0.0),
-    _maximum_damage_increment(getParam<Real>("maximum_damage_increment"))
+    _positive_strain(3, 0.0)
 {
 }
 
 void
 MazarsDamage::initQpStatefulProperties()
 {
-  _damage_index[_qp] = 0.0;
+  ScalarDamageBase::initQpStatefulProperties();
   _kappa[_qp] = 0.0;
 }
 
@@ -97,7 +77,7 @@ MazarsDamage::initialSetup()
 }
 
 void
-MazarsDamage::updateDamage()
+MazarsDamage::updateQpDamageIndex()
 {
   _stress[_qp].symmetricEigenvalues(_eigval);
   for (unsigned int i = 0; i < 3; ++i)
@@ -149,28 +129,4 @@ MazarsDamage::updateDamage()
   // Prevent damage index from decreasing or from being greater than 1
   _damage_index[_qp] = std::max(_damage_index[_qp], _damage_index_old[_qp]);
   _damage_index[_qp] = std::min(_damage_index[_qp], 1.0);
-}
-
-void
-MazarsDamage::updateStressForDamage(RankTwoTensor & stress_new)
-{
-  const Real & damage_index = _use_old_damage ? _damage_index_old[_qp] : _damage_index[_qp];
-  stress_new *= (1.0 - damage_index);
-}
-
-void
-MazarsDamage::updateJacobianMultForDamage(RankFourTensor & jacobian_mult)
-{
-  const Real & damage_index = _use_old_damage ? _damage_index_old[_qp] : _damage_index[_qp];
-  jacobian_mult *= std::max((1.0 - damage_index), _residual_stiffness_fraction);
-}
-
-Real
-MazarsDamage::computeTimeStepLimit()
-{
-  Real current_damage_increment = (_damage_index[_qp] - _damage_index_old[_qp]);
-  if (MooseUtils::absoluteFuzzyEqual(current_damage_increment, 0.0))
-    return std::numeric_limits<Real>::max();
-
-  return _dt * _maximum_damage_increment / current_damage_increment;
 }
