c7dd36ef4f6fa4f4579db83094755de6051e9c4b
Sudipta Biswas <sudipta.biswas@inl.gov>
2020-06-09 08:58:00 -0600

    Correct convection term #125

--- src/kernels/ConcreteThermalConvection.C
+++ src/kernels/ConcreteThermalConvection.C
@@ -21,27 +21,41 @@ InputParameters
 ConcreteThermalConvection::validParams()
 {
   InputParameters params = Kernel::validParams();
+  params.addRequiredCoupledVar("relative_humidity", "nonlinear variable name for rel. humidity");
   return params;
 }
 
 ConcreteThermalConvection::ConcreteThermalConvection(const InputParameters & parameters)
   : Kernel(parameters),
     _cw(getMaterialProperty<Real>("thermal_capacity_of_water")),
-    _darcy_moisture_flux(getMaterialProperty<RealGradient>("darcy_moisture_flux"))
+    _Dh(getMaterialProperty<Real>("humidity_diffusivity")),
+    _grad_rh(coupledGradient("relative_humidity")),
+    _rh_var(coupled("relative_humidity"))
 {
 }
 
 Real
 ConcreteThermalConvection::computeQpResidual()
 {
-  return _cw[_qp] * _darcy_moisture_flux[_qp] * _test[_i][_qp] * _grad_u[_qp];
+  return _cw[_qp] * _Dh[_qp] * _grad_rh[_qp] * _test[_i][_qp] * _grad_u[_qp];
   // return -_specific_heat_water[_qp]*_darcy_mass_flux_water[_qp]*_grad_test[_i][_qp]*_u[_qp];
 }
 
 Real
 ConcreteThermalConvection::computeQpJacobian()
 {
-  return _cw[_qp] * _darcy_moisture_flux[_qp] * _test[_i][_qp] * _grad_phi[_j][_qp];
+  return _cw[_qp] * _Dh[_qp] * _grad_rh[_qp] * _test[_i][_qp] * _grad_phi[_j][_qp];
+  // return
+  // -_specific_heat_water[_qp]*_darcy_mass_flux_water[_qp]*_grad_test[_i][_qp]*_phi[_j][_qp];
+}
+
+Real
+ConcreteThermalConvection::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  if (jvar == _rh_var)
+    return _cw[_qp] * _Dh[_qp] * _grad_u[_qp] * _test[_i][_qp] * _grad_phi[_j][_qp];
+
+  return 0.0;
   // return
   // -_specific_heat_water[_qp]*_darcy_mass_flux_water[_qp]*_grad_test[_i][_qp]*_phi[_j][_qp];
 }
