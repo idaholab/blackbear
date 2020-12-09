6d7543cbbeae2be0e8ce04064c72c4e560b2f1cf
jain651 <jain651@usc.edu>
2020-12-09 10:13:32 -0700

    Revert "Revert "adding new action""
    This reverts commit fd4bcfe2141442f2d5e7cbc041da60f6c859fae8.

--- src/parser/BlackBearSyntax.C
+++ src/parser/BlackBearSyntax.C
@@ -71,5 +71,11 @@ associateSyntax(Syntax & syntax, ActionFactory & /*action_factory*/)
       "SetReactionNetworkAction", "ReactionNetwork", "add_minerals_kernels");
   syntax.registerActionSyntax(
       "SetReactionNetworkAction", "ReactionNetwork", "add_minerals_auxkernels");
+
+  registerSyntax("EmptyAction", "Constraints/EqualValueEmbeddedConstraint");
+  registerSyntax("EqualValueEmbeddedConstraintAction",
+                 "Constraints/EqualValueEmbeddedConstraint/*");
+
+
 }
 } // namespace BlackBear
