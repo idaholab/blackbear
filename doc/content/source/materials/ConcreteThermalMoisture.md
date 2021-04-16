3fd522c1978763f25287f557fb8f9f696b16e234
jain651 <jain651@usc.edu>
2021-04-16 14:23:05 -0600

    Correcting functions, adding warning statement, and expiration date

--- doc/content/source/materials/ConcreteThermalMoisture.md
+++ doc/content/source/materials/ConcreteThermalMoisture.md
@@ -1,18 +1,18 @@
 <!-- MOOSE Documentation Stub: Remove this when content is added. -->
 
-# PorousMediaBase
+# ConcreteThermalMoisture
 
 !alert construction title=Undocumented Class
-The PorousMediaBase has not been documented. The content contained on this page includes the
+The ConcreteThermalMoisture has not been documented. The content contained on this page includes the
 typical automatic documentation associated with a MooseObject; however, what is contained is
 ultimately determined by what is necessary to make the documentation clear for users.
 
-!syntax description /Materials/PorousMediaBase
+!syntax description /Materials/ConcreteThermalMoisture
 
-!syntax parameters /Materials/PorousMediaBase
+!syntax parameters /Materials/ConcreteThermalMoisture
 
-!syntax inputs /Materials/PorousMediaBase
+!syntax inputs /Materials/ConcreteThermalMoisture
 
-!syntax children /Materials/PorousMediaBase
+!syntax children /Materials/ConcreteThermalMoisture
 
 !bibtex bibliography
--- src/materials/ConcreteThermalMoisture.C
+++ src/materials/ConcreteThermalMoisture.C
@@ -12,15 +12,20 @@
 /*            See COPYRIGHT for full restrictions               */
 /****************************************************************/
 
-#include "PorousMediaBase.h"
+#include "ConcreteThermalMoisture.h"
 
 // libMesh includes
 #include "libmesh/quadrature.h"
 
-registerMooseObject("BlackBearApp", PorousMediaBase);
+registerMooseObject("BlackBearApp", ConcreteThermalMoisture);
+
+registerMooseObjectRenamed("BlackBearApp",
+                           PorousMediaBase,
+                           "06/15/2021 00:01",
+                           ConcreteThermalMoisture);
 
 InputParameters
-PorousMediaBase::validParams()
+ConcreteThermalMoisture::validParams()
 {
   InputParameters params = Material::validParams();
 
@@ -93,7 +98,7 @@ PorousMediaBase::validParams()
   return params;
 }
 
-PorousMediaBase::PorousMediaBase(const InputParameters & parameters)
+ConcreteThermalMoisture::ConcreteThermalMoisture(const InputParameters & parameters)
   : Material(parameters),
     _thermal_conductivity_model(getParam<MooseEnum>("thermal_conductivity_model")),
     _thermal_capacity_model(getParam<MooseEnum>("thermal_capacity_model")),
@@ -167,17 +172,29 @@ PorousMediaBase::PorousMediaBase(const InputParameters & parameters)
   _vals.resize(n);
   for (unsigned int i = 0; i < _vals.size(); ++i)
     _vals[i] = &coupledValue("mineral_compositions", i);
+
+  if (getParam<std::string>("type") == "PorousMediaBase")
+    out << "*********************************************************************************\n"
+        << "************************************ Warning ************************************\n"
+        << "PorousMediaBase is used to obtain thermal and moisture properties of concrete. \n"
+        << "Note that the following function definitions are corrected for temperature <= "
+           "20\u2103:\n"
+        << "  * thermal capacity for ASCE-1992 and KODUR-2004 models and\n"
+        << "  * thermal conductivity for ASCE-1992, KODUR-2004, and EUROCODE-2004 models.\n"
+        << "N_wc definition is also corrected for water-cement ratio <= 0.3.\n"
+        << "*********************************************************************************"
+        << std::endl;
 }
 
 void
-PorousMediaBase::initQpStatefulProperties()
+ConcreteThermalMoisture::initQpStatefulProperties()
 {
   _te[_qp] = _cure_time;
   _eqv_age[_qp] = _cure_time;
 }
 
 void
-PorousMediaBase::computeProperties()
+ConcreteThermalMoisture::computeProperties()
 {
   for (unsigned int qp = 0; qp < _qrule->n_points(); ++qp)
   {
@@ -247,7 +264,7 @@ PorousMediaBase::computeProperties()
         {
           case 0: // siliceous aggreagte
             if (T < 20.0)
-              _thermal_capacity[qp] = ro * Cv;
+              _thermal_capacity[qp] = 1.8 * 1e6;
             else if (T >= 20.0 && T < 200.0)
               _thermal_capacity[qp] = (0.005 * T + 1.7) * 1.0e6;
             else if (T >= 200.0 && T < 400.0)
@@ -261,9 +278,7 @@ PorousMediaBase::computeProperties()
             break;
 
           case 1: // carbonate aggregate
-            if (T < 20.0)
-              _thermal_capacity[qp] = ro * Cv;
-            else if (T >= 20.0 && T < 400.0)
+            if (T < 400.0)
               _thermal_capacity[qp] = 2.566 * 1.0e6;
             else if (T >= 400.0 && T < 410.0)
               _thermal_capacity[qp] = (0.1765 * T - 68.034) * 1.0e6;
@@ -292,7 +307,7 @@ PorousMediaBase::computeProperties()
         {
           case 0: // siliceous aggreagte
             if (T < 20.0)
-              _thermal_capacity[qp] = ro * Cv;
+              _thermal_capacity[qp] = 1.8 * 1.0e6;
             else if (T >= 20.0 && T < 200.0)
               _thermal_capacity[qp] = (0.005 * T + 1.7) * 1.0e6;
             else if (T >= 200.0 && T < 400.0)
@@ -309,9 +324,7 @@ PorousMediaBase::computeProperties()
             break;
 
           case 1: // carbonate aggregate
-            if (T < 20.0)
-              _thermal_capacity[qp] = ro * Cv;
-            else if (T >= 20.0 && T < 400.0)
+            if (T < 400.0)
               _thermal_capacity[qp] = 2.45 * 1.0e6;
             else if (T >= 400.0 && T < 475.0)
               _thermal_capacity[qp] = (0.026 * T - 12.85) * 1.0e6;
@@ -377,7 +390,7 @@ PorousMediaBase::computeProperties()
         {
           case 0: // siliceous aggreagte
             if (T < 20.0)
-              _thermal_conductivity[qp] = _input_thermal_conductivity_of_concrete;
+              _thermal_conductivity[qp] = 1.4875;
             else if (T >= 20.0 && T < 800.0)
               _thermal_conductivity[qp] = -0.000625 * T + 1.5;
             else if (T >= 800.0)
@@ -385,11 +398,9 @@ PorousMediaBase::computeProperties()
             break;
 
           case 1: // carbonate aggregate
-            if (T < 20.0)
-              _thermal_conductivity[qp] = _input_thermal_conductivity_of_concrete;
-            else if (T >= 20.0 && T < 293.0)
+            if (T < 293.0)
               _thermal_conductivity[qp] = 1.355;
-            else if (T >= 293.0)
+            else
               _thermal_conductivity[qp] = -0.001241 * T + 1.7162;
             break;
 
@@ -404,7 +415,7 @@ PorousMediaBase::computeProperties()
         {
           case 0: // siliceous aggreagte
             if (T < 20.0)
-              _thermal_conductivity[qp] = _input_thermal_conductivity_of_concrete;
+              _thermal_conductivity[qp] = 1.4875;
             else if (T >= 20.0 && T < 800.0)
               _thermal_conductivity[qp] = -0.000625 * T + 1.5;
             else if (T >= 800.0)
@@ -412,11 +423,9 @@ PorousMediaBase::computeProperties()
             break;
 
           case 1: // carbonate aggregate
-            if (T < 20.0)
-              _thermal_conductivity[qp] = _input_thermal_conductivity_of_concrete;
-            else if (T >= 20.0 && T < 293.0)
+            if (T < 293.0)
               _thermal_conductivity[qp] = 1.355;
-            else if (T >= 293.0)
+            else
               _thermal_conductivity[qp] = -0.001241 * T + 1.7162;
             break;
 
@@ -427,7 +436,7 @@ PorousMediaBase::computeProperties()
         break;
       case 3: // EUROCODE-2004
         if (T < 20.0)
-          _thermal_conductivity[qp] = _input_thermal_conductivity_of_concrete;
+          _thermal_conductivity[qp] = 1.642218;
         else if (T >= 20.0 && T <= 1200.0)
         {
           const Real k_up = 2.0 - 0.2451 * (T / 100.0) + 0.0107 * std::pow(T / 100.0, 2.0);
@@ -491,7 +500,7 @@ PorousMediaBase::computeProperties()
 
     Real N_wc = 0.9;
     if (_water_to_cement < 0.3)
-      N_wc = 0.9;
+      N_wc = 0.99;
     else if (_water_to_cement >= 0.3 && _water_to_cement <= 0.7)
       N_wc = 0.33 + 2.2 * _water_to_cement;
     else
