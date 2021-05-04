17861794a5e8fb7374cee2177a2cb405af571fba
Ben Spencer <benjamin.spencer@inl.gov>
2021-05-04 13:55:47 -0600

    Update test for concrete thermal/moisture transport closes #216
    1) Delete the existing 2D test
    2) Add a coarse, fast-running 1D test of the MAQBETH experiment. This
       runs with larger time steps and a coarser mesh than we would prefer,
       but the results are reasonably close to a more refined version, and
       reasonably close to the published experimental results.
    3) Add a Python script for plotting the results of this test at various
       times reported in the paper on this experiment.

---
+++ test/tests/concrete_moisture_heat_transfer/temp_rh_plots.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import pandas
+import mooseutils
+import matplotlib.pyplot as plt
+
+f1 = plt.figure(1, figsize=(6,4.5))
+ax = plt.gca()
+data = mooseutils.VectorPostprocessorReader('csv/out_profiles_*.csv')
+
+t_times = [19.5, 30.5, 41.8, 61.3, 125.0, 194.6]
+rh_times = [33.4, 58.4, 66.7, 77.9, 152.8, 194.5]
+
+#T = data('T', time = 0)
+#print(T)
+#print(data.data.keys().values)
+
+#Plot a line for every output time:
+#for time in data.data.data.keys().values:
+#  data.data.data[time].plot(ax=ax,x='x',y='T')
+
+#Plot lines for selected interpolated output times:
+for t_hr in t_times:
+  t = t_hr * 3600
+  data.update(time=t)
+  data.data.data.plot(ax=ax,x='id',y='T',label=str(t_hr)+' hr')
+
+plt.xlabel('Radial Position (m)')
+plt.ylabel('Temperature ($\degree$C)')
+
+plt.savefig('temp_plot.pdf')
+
+f2 = plt.figure(2, figsize=(6,4.5))
+ax = plt.gca()
+
+for t_hr in rh_times:
+  t = t_hr * 3600
+  data.update(time=t)
+  data.data.data.plot(ax=ax,x='id',y='rh',label=str(t_hr)+' hr')
+
+plt.xlabel('Radial Position (m)')
+plt.ylabel('$h_r$')
+
+plt.savefig('rh_plot.pdf')
