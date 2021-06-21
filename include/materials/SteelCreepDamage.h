319d65afe3a267bb1d5f464e6ad2729927549935
Antonio Recuero <amrecuero85@gmail.com>
2021-06-21 17:57:03 -0600

    Address Daniel's review: Add doxygen comments

--- include/materials/SteelCreepDamage.h
+++ include/materials/SteelCreepDamage.h
@@ -25,6 +25,7 @@ class SteelCreepDamageTempl : public ScalarDamageBaseTempl<is_ad>, public Guaran
 {
 public:
   static InputParameters validParams();
+
   SteelCreepDamageTempl(const InputParameters & parameters);
 
   virtual void initQpStatefulProperties() override;
@@ -48,19 +49,34 @@ protected:
 
   std::string _creep_strain_name;
 
+  ///@{ Creep strain tensors
   const GenericMaterialProperty<RankTwoTensor, is_ad> & _creep_strain;
   const MaterialProperty<RankTwoTensor> & _creep_strain_old;
+  ///@}
 
+  /// Uniaxial creep fracture strain (used to compute multiaxial creep fracture strain)
+  /// This parameter usually take values that range from 0.05 to 0.2
+  /// "Creep failure simulations of 316H at 550C: Part I - A method and
+  /// validation", Oh et al, Engineering Fracture Mechanics 78 (2011)
   const Real _epsilon_f;
 
+  /// Positive real to reduce the amount of stress at the quadrature point.
+  /// The larger the the value, the less the load-carrying capacity of the element will be.
   const Real _reduce_factor;
 
+  /// Initial damage value at which we will start reducing linearly the element's load
+  ///carrying capacity. initial_damage_value < 1
   const Real _initial_damage_value;
 
+  /// Value to match the power law creep model used as a material
+  /// If material is defined by multiple creep models, this value represents
+  /// an average of creep behavior.
   const Real _creep_law_exponent;
 
+  /// Generic stress tensor
   const GenericMaterialProperty<RankTwoTensor, is_ad> & _stress;
 
+  ///@{ Make hierarchy parameters available in this class
   using ScalarDamageBaseTempl<is_ad>::_damage_index;
   using ScalarDamageBaseTempl<is_ad>::_damage_index_name;
   using ScalarDamageBaseTempl<is_ad>::_damage_index_old;
@@ -70,6 +86,7 @@ protected:
   using ScalarDamageBaseTempl<is_ad>::_dt;
   using ScalarDamageBaseTempl<is_ad>::_base_name;
   using ScalarDamageBaseTempl<is_ad>::_maximum_damage_increment;
+  ///@}
 };
 
 typedef SteelCreepDamageTempl<false> SteelCreepDamage;
